e6bdd0131d348b978143448acac0e84cb6220594
Niks Birzgalis <75217926+birzgalisn@users.noreply.github.com>
2024-03-27 10:02:21 +0200

    Remove unused forecast hub methods

--- api/Hubs/ForecastHub.cs
+++ api/Hubs/ForecastHub.cs
@@ -6,8 +6,6 @@ namespace Api.Hubs;
 public interface IForecastHub
 {
     public Task NewForecast(WeatherForecast forecast);
-
-    public Task FahrenheitCalculated(int forecastId, double? fahrenheit);
 }
 
 public class ForecastHub : Hub<IForecastHub>
@@ -15,10 +13,5 @@ public class ForecastHub : Hub<IForecastHub>
     public async Task NewForecast(WeatherForecast forecast)
     {
         await Clients.All.NewForecast(forecast);
-    }
-
-    public async Task FahrenheitCalculated(int forecastId, double fahrenheit)
-    {
-        await Clients.All.FahrenheitCalculated(forecastId, fahrenheit);
     }
 }
