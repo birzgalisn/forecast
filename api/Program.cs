9da41dbca9035dcbf42c85f882b6ade56a10ed0d
Niks Birzgalis <75217926+birzgalisn@users.noreply.github.com>
2024-03-28 18:02:37 +0200

    Improvements

--- api/Program.cs
+++ api/Program.cs
@@ -11,13 +11,11 @@ builder.Services.AddSignalR();
 builder.Services.AddDbContext<ForecastDbContext>(options => options.UseNpgsql(Environment.GetEnvironmentVariable("DB_CONNECTION_STRING") ?? builder.Configuration.GetConnectionString("Db")));
 builder.Services.AddTransient<IStartupFilter, DataContextAutomaticMigrationStartupFilter<ForecastDbContext>>();
 
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -27,8 +25,8 @@ if (app.Environment.IsDevelopment())
 app.UseCors(options => options
     .AllowAnyMethod()
     .AllowAnyHeader()
-    .SetIsOriginAllowed(origin => true) // Allow any origin
-    .AllowCredentials()); // Allow credentials
+    .SetIsOriginAllowed(origin => true)
+    .AllowCredentials());
 
 app.UseAuthorization();
 
